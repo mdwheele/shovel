f49913bc92b976a4d5026f453b59848e6bbc4d7f
Dustin Wheeler <mdwheele@ncsu.edu>
2017-03-08 06:42:28 +0000

    Passing test using the crack-addled PDO_OCI hackery

--- docs/REFERENCES.md
+++ docs/REFERENCES.md
@@ -19,6 +19,7 @@
 ## PHP / Oracle
 
 - [Installing OCI8 Ubuntu / PHP 56](http://www.syahzul.com/2016/04/06/how-to-install-oci8-on-ubuntu-14-04-and-php-5-6)
+- [Compiling PDO_OCI](http://stackoverflow.com/questions/30608027/php-pdo-oci-driver-installation-using-pecl) This actually seems to come packages in remi's SCL package.
 
 ## Configuration
 
--- tests/integration/OCITalksToOracleTest.php
+++ tests/integration/OCITalksToOracleTest.php
@@ -31,12 +31,23 @@ class OCITalksToOracleTest extends \PHPUnit\Framework\TestCase
     }
 
     /** @test */
-    public function it_can_query_the_database()
+    public function it_can_query_the_database_using_oci_functions()
     {
         $stmt = $this->query('SELECT * FROM phpunit');
         oci_execute($stmt);
 
         $row = oci_fetch_assoc($stmt);
+
+        $this->assertEquals('Hello, World!', $row['MSG']);
+    }
+
+    /** @test */
+    public function it_can_use_rando_pdo_hackery()
+    {
+        $pdo = new \PDOOCI\PDO('oci:dbname=//localhost:1521/xe.oracle.docker', 'system', 'oracle');
+
+        $stmt = $pdo->query('SELECT * FROM phpunit');
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
         $this->assertEquals('Hello, World!', $row['MSG']);
     }
